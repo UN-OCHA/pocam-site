9deaeca1b01fb1a8befe3ea0106ad2a44faa336a
Peter Droogmans (attiks) <peter@attiks.com>
2018-06-13 13:32:54 +0200

    Host fonts locally

--- html/sites/all/themes/custom/ochabasic/templates/html.tpl.php
+++ html/sites/all/themes/custom/ochabasic/templates/html.tpl.php
@@ -12,20 +12,11 @@
   <title><?php print $head_title; ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php print $styles; ?>
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet">
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $scripts; ?>
   <?php print $page_bottom; ?>
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-101476383-1', 'auto');
-    ga('send', 'pageview');
-  </script>
 </body>
 </html>
