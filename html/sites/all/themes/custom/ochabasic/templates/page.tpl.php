e6dda5b74045a498b27fdc31bc7be61a62ac5689
Peter Droogmans (attiks) <peter@attiks.com>
2018-06-13 09:42:49 +0200

    Styling search page

--- html/sites/all/themes/custom/ochabasic/templates/page.tpl.php
+++ html/sites/all/themes/custom/ochabasic/templates/page.tpl.php
@@ -9,28 +9,30 @@
 unset($page['content']['system_main']['default_message']);
 
 ?>
-<div class="page-wrapper">
-  <?php include 'header.inc'; ?>
+  <div class="page-wrapper">
+    <?php include 'header.inc'; ?>
 
-  <?php if($messages): ?>
+    <?php if($messages): ?>
     <div id="messages" class="container">
       <?php print $messages; ?>
     </div>
-  <?php endif; ?>
-
-  <div id="main" class="container">
-
-    <?php if ($tabs): ?>
-      <div class="tabs"><?php print render($tabs); ?></div>
     <?php endif; ?>
 
-    <?php if (!drupal_is_front_page() && $title && isset($node) && $node->type !== 'ev_event'): ?>
+    <div id="main" class="container">
+      <?php if ($tabs): ?>
+        <div class="tabs">
+          <?php print render($tabs); ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if (!drupal_is_front_page() && $title && isset($node) && $node->type !== 'ev_event'): ?>
       <h1>
         <?php print $title; ?>
       </h1>
-    <?php endif; ?>
+      <?php endif; ?>
 
-     <?php print render($page['content']); ?>
+      <?php print render($page['content_top']); ?>
+      <?php print render($page['content']); ?>
+    </div>
   </div>
-</div>
-<?php include 'footer.inc'; ?>
+  <?php include 'footer.inc'; ?>
