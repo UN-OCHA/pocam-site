caaf9224ab676bfd5fefbf7727d1dd6f76ae7a41
Peter Droogmans (attiks) <peter@attiks.com>
2018-06-21 16:37:23 +0200

    Limit text on mobile

---
+++ html/sites/all/themes/custom/ochabasic/assets/js/script.js
@@ -0,0 +1,19 @@
+(function ($) {
+  Drupal.behaviors.ocha = {
+    attach: function (context, settings) {
+      'use strict';
+
+      // Add class to all p tags.
+      $('.views-field-field-text-value p').addClass('collapsed');
+
+      // Add click handler.
+      $('.data--read-more').click(function (e) {
+        $(e.target)
+          .parent()
+          .find('.views-field-field-text-value p')
+          .toggleClass('collapsed');
+      });
+    }
+  };
+})(jQuery);
+  
