58b38824ac378aef022e68fdea5a25d499525b90
Peter Droogmans (attiks) <peter@attiks.com>
2018-06-21 21:09:21 +0200

    CS

--- html/sites/all/themes/custom/ochabasic/templates/views-view-table--extracts.tpl.php
+++ html/sites/all/themes/custom/ochabasic/templates/views-view-table--extracts.tpl.php
@@ -20,15 +20,15 @@
  * @ingroup views_templates
  */
 ?>
-<table <?php if ($classes): ?> class="<?php print $classes; ?>"<?php endif ?><?php print $attributes; ?>>
-   <?php if (!empty($title) || !empty($caption)): ?>
-     <caption><?php print $caption . $title; ?></caption>
+<table class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php if (!empty($title) || !empty($caption)): ?>
+    <caption><?php print $caption . $title; ?></caption>
   <?php endif; ?>
   <?php if (!empty($header)) : ?>
     <thead>
       <tr>
         <?php foreach ($header as $field => $label): ?>
-          <th <?php if ($header_classes[$field]): ?> class="<?php print $header_classes[$field]; ?>"<?php endif; ?> scope="col">
+          <th class="<?php print $header_classes[$field]; ?>" scope="col">
             <?php print $label; ?>
           </th>
         <?php endforeach; ?>
@@ -37,9 +37,9 @@
   <?php endif; ?>
   <tbody>
     <?php foreach ($rows as $row_count => $row): ?>
-      <tr <?php if ($row_classes[$row_count]): ?> class="<?php print implode(' ', $row_classes[$row_count]); ?>"<?php endif; ?>>
+      <tr class="<?php print implode(' ', $row_classes[$row_count]); ?>">
         <?php foreach ($row as $field => $content): ?>
-          <td <?php if ($field_classes[$field][$row_count]): ?> class="<?php print $field_classes[$field][$row_count]; ?>"<?php endif; ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+          <td class="<?php print $field_classes[$field][$row_count]; ?>" <?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
             <?php print $content; ?>
           </td>
         <?php endforeach; ?>
