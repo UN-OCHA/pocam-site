d5885e6a3b59986958f6f3cc908e8de344cd357b
left23 <lcdemc@gmail.com>
2018-08-03 09:47:28 +0200

    update menu from ocha_basic recent improvements

--- html/sites/all/themes/custom/ochabasic/js/custom-menu.js
+++ html/sites/all/themes/custom/ochabasic/js/custom-menu.js
@@ -1,10 +1,10 @@
 /**
  * @file
- * Main menu with boostrap dropdown component.
+ * Bootstrap dropdown on main menu.
  */
 
-(function ($) {
-  Drupal.behaviors.exampleModule = {
+(function ($, Drupal) {
+  Drupal.behaviors.cdMenu = {
     attach: function (context, settings) {
 
       $('.cd-dropdown .expanded a').on('click', function (e) {
@@ -15,4 +15,4 @@
 
     }
   };
-}(jQuery));
+}(jQuery, Drupal));
